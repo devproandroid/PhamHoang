cb704951ced31c7887d2cccab9731f47805761a0
Admin <Admin@192.168.0.105>
2018-05-10 13:18:31 +0700

    First Commit

---
+++ src/com/trainning/homeWork.java
@@ -0,0 +1,10 @@
+package com.trainning;
+
+public class homeWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my homework");
+	}
+
+}
